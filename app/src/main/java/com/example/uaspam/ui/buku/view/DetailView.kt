6c0f4c83218f5592ec4de88e5728953f840a17c8
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-27 18:25:13 +0700

    memperbarui kode

---
+++ app/src/main/java/com/example/uaspam/ui/buku/view/DetailView.kt
@@ -0,0 +1,244 @@
+package com.example.uaspam.ui.buku.view
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.model.Buku
+import com.example.uaspam.ui.buku.viewmodel.DetailBukuViewModel
+import com.example.uaspam.ui.PenyediaViewModel
+import com.example.uaspam.ui.buku.viewmodel.DetailUiState
+import com.example.uaspam.ui.buku.viewmodel.toBuku
+import com.example.uaspam.ui.customewidget.CostumeTopAppBar
+import com.example.uaspam.ui.navigation.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Buku"
+    const val IDBUKU = "idBuku"
+    val routeWithArgs = "$route/{$IDBUKU}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit = { },
+    viewModel: DetailBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Buku",
+                )
+            }
+        }
+    ) { innerPadding ->
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        BodyDetailBuku(
+            detailUiState = viewModel.detailUiState,
+            modifier = Modifier.padding(innerPadding),
+            onDeleteClick = {
+                deleteConfirmationRequired = true
+            }
+        )
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    viewModel.deleteBuku()
+                    onDeleteClick()
+                    deleteConfirmationRequired = false
+                },
+                onDeleteCancel = {
+                    deleteConfirmationRequired = false
+                },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun BodyDetailBuku(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState,
+    onDeleteClick: () -> Unit
+) {
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        detailUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = detailUiState.errorMessage,
+                    color = Color.Red
+                )
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailBuku(
+                    buku = detailUiState.detailUiEvent.toBuku(),
+                    modifier = modifier
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = onDeleteClick,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ItemDetailBuku(
+    modifier: Modifier = Modifier,
+    buku: Buku,
+){
+
+    // Log the idBuku
+    Log.d("BookID", "Book ID: ${buku.idBuku}")
+
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            ComponentDetailBuku(judul = "Id Buku", isinya = buku.idBuku.toString())
+            ComponentDetailBuku(judul = "Nama Buku", isinya = buku.namaBuku)
+            ComponentDetailBuku(judul = "Deskripsi Buku", isinya = buku.deskripsiBuku)
+            ComponentDetailBuku(judul = "Tanggal Buku", isinya = buku.tanggalTerbit)
+            ComponentDetailBuku(judul = "Status Buku", isinya = buku.statusBuku)
+            ComponentDetailBuku(judul = "Id Kategori", isinya = buku.idKategori.toString())
+            ComponentDetailBuku(judul = "Id Penulis", isinya = buku.idPenulis.toString())
+            ComponentDetailBuku(judul = "Id Penerbit", isinya = buku.idPenerbit.toString())
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailBuku(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "$judul:",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/example/uaspam/ui/buku/view/HomeBukuView.kt
@@ -0,0 +1,291 @@
+package com.example.uaspam.ui.buku.view
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
+import com.example.uaspam.model.Buku
+import com.example.uaspam.ui.buku.viewmodel.HomeBukuViewModel
+import com.example.uaspam.ui.buku.viewmodel.HomeuiState
+import com.example.uaspam.ui.PenyediaViewModel
+import com.example.uaspam.ui.customewidget.CostumeTopAppBar
+import com.example.uaspam.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Buku"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeBukuView(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    viewModel: HomeBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    var selectedBuku: Buku? by remember { mutableStateOf(null) }
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getBuku()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            )
+            {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Buku" )
+            }
+        },
+    ){innerPadding ->
+        HomeStatus(
+            homeuiState = viewModel.bukuUiState,
+            retryAction = { viewModel.getBuku()},
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {buku ->
+                selectedBuku = buku
+                showDeleteDialog = true
+            }
+        )
+
+        if (showDeleteDialog && selectedBuku != null) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    selectedBuku?.let { buku ->
+                        viewModel.deleteBuku(buku.idBuku.toString())
+                    }
+                    showDeleteDialog = false
+                },
+                onDeleteCancel = {
+                    showDeleteDialog = false
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeuiState: HomeuiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Buku) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when (homeuiState){
+        is HomeuiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeuiState.Success ->
+            if (homeuiState.buku.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data Buku")
+                }
+            }else {
+                BukuLayout(
+                    buku = homeuiState.buku,
+                    modifier=modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.toString())
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeuiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier
+            .size(100.dp)
+            .padding(40.dp),
+        painter = painterResource(R.drawable.gambar),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(id = R.drawable.gambarlagi), contentDescription = ""
+        )
+        Text(text = stringResource(id = R.string.loading_failed),
+            modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun BukuLayout(
+    buku: List<Buku>,
+    modifier: Modifier= Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Buku) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(buku) { buku ->
+            BukuCard(
+                buku = buku,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(buku.idBuku.toString()) },
+                onDeleteClick = {
+                    onDeleteClick(buku)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun BukuCard(
+    buku: Buku,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Buku) -> Unit = {}
+){
+    Card (
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = buku.namaBuku,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = {
+                    Log.d("BookID", "Selected Buku ID: ${buku.idBuku}")
+                    onDeleteClick(buku) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = buku.deskripsiBuku,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = buku.tanggalTerbit,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = buku.statusBuku,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = buku.idKategori.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = buku.idPenulis.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = buku.idPenerbit.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {/*Do Nothing*/},
+        title = {Text("Delete Data")},
+        text = {Text("Apakah anda yakin ingin menghapus data ini?")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text("Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text("Yes")
+            }
+        }
+    )
+}
+
