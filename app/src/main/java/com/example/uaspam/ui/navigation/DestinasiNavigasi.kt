627b4afd8f4d30f04c25d3e3a076dc4d48b7c790
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 12:14:26 +0700

    menambah detailhalaman home ke dalam DestinasiNavigasi

--- app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,26 @@ interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
+
+object DestinasiHalamanHome : DestinasiNavigasi {
+    override val route = "mainScreen"
+    override val titleRes = "Main Screen"
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Buku"
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Buku"
+    const val IDBUKU = "idBuku"
+    val routeWithArgs = "$route/{$IDBUKU}"
+}
+
+object DestinasiUpdate: DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Buku"
+}
+
