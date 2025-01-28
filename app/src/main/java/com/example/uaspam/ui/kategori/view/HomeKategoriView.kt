5bba7f4c7543cf874e40327eb8ad754d4291535f
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:11:01 +0700

    membuat objek destinasi kategori untuk route ke halaman home kategori

---
+++ app/src/main/java/com/example/uaspam/ui/kategori/view/HomeKategoriView.kt
@@ -0,0 +1,58 @@
+package com.example.uaspam.ui.kategori.view
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
+import com.example.uaspam.model.Kategori
+import com.example.uaspam.ui.PenyediaViewModel
+import com.example.uaspam.ui.navigation.CostumeTopAppBar
+import com.example.uaspam.ui.kategori.viewmodel.HomeKategoriViewModel
+import com.example.uaspam.ui.kategori.viewmodel.HomeuiState
+import com.example.uaspam.ui.navigation.DestinasiNavigasi
+
+object DestinasiKategori : DestinasiNavigasi {
+    override val route = "kategori"
+    override val titleRes = "Home Kategori"
+}
+
