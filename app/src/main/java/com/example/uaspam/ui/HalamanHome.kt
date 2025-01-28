81013ffb7c4ee843337ae31de34c0932776b91c0
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 12:17:01 +0700

    menambahkan Halaman Home untuk mengeset Tampilan Ui nya

---
+++ app/src/main/java/com/example/uaspam/ui/HalamanHome.kt
@@ -0,0 +1,139 @@
+package com.example.uaspam.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
+import com.example.uaspam.ui.ViewModel.HalamanHomeViewModel
+import com.example.uaspam.ui.navigation.DestinasiNavigasi
+import com.example.uaspam.ui.navigation.TopAppBarHome
+
+object DestinasiHalamanHome : DestinasiNavigasi {
+    override val route = "halamanhome"
+    override val titleRes = "Halaman Utama"
+}
+
+
+@Composable
+fun HalamanHome(
+    viewModel: HalamanHomeViewModel = viewModel(),
+    onNavigateToBuku: () -> Unit = {},
+    onNavigateToKategori: () -> Unit = {},
+    onNavigateToPenulis: () -> Unit ={},
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBarHome()
+        },
+        modifier = modifier
+    )
+
+    {
+
+            innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = "Selamat Datang di Toko",
+                    modifier = Modifier.padding(30.dp),
+                    color = colorResource(id = R.color.purple_200),
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Button(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(start = 40.dp, end = 40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.purple_500)
+                    ),
+                    onClick = { viewModel.navigateToBuku() },
+
+                    ) {
+                    Text(
+
+                        text = "Buku")
+                }
+
+                Button(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(start = 40.dp, end = 40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.purple_500)),
+                    onClick = { viewModel.navigateToKategori() }
+                ) {
+                    Text(text = "Kategori")
+                }
+
+                Button(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(start = 40.dp, end = 40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.purple_500)),
+                    onClick = { viewModel.navigateToPenulis() }
+                ) {
+                    Text(text = "Penulis")
+                }
+
+                Button(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(start = 40.dp, end = 40.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.purple_500)),
+                    onClick = { viewModel.navigateToPenerbit() }
+                ) {
+                    Text(text = "Penerbit")
+                }
+            }
+        }
+    }
+
+
+
+    LaunchedEffect(uiState.navigateToBuku) {
+        if (uiState.navigateToBuku) {
+            viewModel.resetNavigation()
+            onNavigateToBuku()
+        }
+    }
+
+    LaunchedEffect(uiState.navigateToKategori) {
+        if (uiState.navigateToKategori) {
+            viewModel.resetNavigation()
+            onNavigateToKategori()
+        }
+    }
+}
--- app/src/main/java/com/example/uaspam/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/uaspam/ui/navigation/PengelolaHalaman.kt
@@ -8,40 +8,103 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.uaspam.ui.HalamanHome
+import com.example.uaspam.ui.buku.view.DestinasiDetail
 import com.example.uaspam.ui.buku.view.DestinasiEntry
-import com.example.uaspam.ui.buku.view.DestinasiHome
-import com.example.uaspam.ui.buku.view.DetailDestinasi
-import com.example.uaspam.ui.buku.view.DetailScreen
+import com.example.uaspam.ui.buku.view.DestinasiUpdate
+import com.example.uaspam.ui.buku.view.DetailView
 import com.example.uaspam.ui.buku.view.HomeBukuView
 import com.example.uaspam.ui.buku.view.InsertBukuView
+import com.example.uaspam.ui.buku.view.UpdateScreen
+import com.example.uaspam.ui.kategori.view.HomeKategoriView
 
 @Composable
 fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ){
     NavHost(
         navController = navController,
-        startDestination = DestinasiHome.route,
+        startDestination = DestinasiHalamanHome.route,
         modifier = Modifier,
-    ){
-        composable(DestinasiHome.route){
-            HomeBukuView(
-                navigateToltemEntry = { navController.navigate(DestinasiEntry.route) },
-                onDetailClick = {
+    ) {
+
+        composable(DestinasiHalamanHome.route){
+            HalamanHome(
+                onNavigateToBuku = {
+                    navController.navigate(DestinasiHome.route)
+                },
+                onNavigateToKategori = {
+                    navController.navigate(DestinasiHome.route)
                 }
             )
         }
-
+        composable(DestinasiHome.route)
+        {
+            HomeBukuView(
+                onBackClick = {navController.navigate(DestinasiHalamanHome.route)},
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = { idBuku ->
+                    navController.navigate("${DestinasiDetail.route}/$idBuku")
+                }
+            )
+        }
         composable(DestinasiEntry.route) {
             InsertBukuView(
                 navigateBack = {
-                    navController.navigate(DestinasiHome.route){
-                        popUpTo(DestinasiHome.route){
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
                             inclusive = true
                         }
                     }
                 }
             )
         }
+
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.IDBUKU) {
+                    type = NavType.StringType })
+        ) { backStackEntry ->
+            val idBuku = backStackEntry.arguments?.getString(DestinasiDetail.IDBUKU) ?: ""
+            DetailView(
+                navigateBack = { navController.navigateUp() },
+                onEditClick = { navController.navigate("update") },
+                onDeleteClick = { navController.navigateUp() }
+//                idBuku = idBuku // Pass the idBuku to the Detail View
+            )
+        }
+
+        composable(
+            DestinasiUpdate.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiUpdate.IDBUKU) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            UpdateScreen(
+                NavigateBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+            )
+        }
+
+        // tabel kategori
+
+//        composable(DestinasiHome.route)
+//        {
+//            HomeKategoriView(
+////                onBackClick = {navController.navigate(DestinasiHalamanHome.route)},
+//                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+//                onDetailClick = { idKategori ->
+//                    navController.navigate("${DestinasiDetail.route}/$idKategori")
+//                }
+//            )
+//        }
     }
 }
