c595f9bc8058fc7e747e4ff3c0e3990ea9d05e5c
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:30:12 +0700

    menambah sealed class untuk merepresentasikan berbagai status dalam konteks pengambilan data

---
+++ app/src/main/java/com/example/uaspam/ui/kategori/viewmodel/HomeKategoriViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.uaspam.ui.kategori.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.uaspam.model.Kategori
+import com.example.uaspam.repository.KategoriRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class HomeuiState{
+    data class Success(val kategori: List<Kategori>): HomeuiState()
+    object Error: HomeuiState()
+    object Loading: HomeuiState()
+}
+
