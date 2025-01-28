37efc01e0da137b85c749e2043cf86aca3a971b0
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:37:49 +0700

    membuat class dan state untuk menyimpan data yang sedang diperbarui dan mengamnil id kategori berdasarkan id

---
+++ app/src/main/java/com/example/uaspam/ui/kategori/viewmodel/UpdateKategoriViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.uaspam.ui.kategori.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.repository.KategoriRepository
+import com.example.uaspam.ui.kategori.view.DestinasiUpKategori
+import kotlinx.coroutines.launch
+
+class UpdateKategoriViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val kategoriRepository: KategoriRepository)
+    : ViewModel() {
+
+        var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+                private val _idKategori: String = checkNotNull(savedStateHandle[DestinasiUpKategori.IDKATEGORI])
+
+        init {
+            viewModelScope.launch {
+                try {
+                    updateUiState = kategoriRepository.getKategoribyId(_idKategori)
+                        .toUiStateKategori()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
