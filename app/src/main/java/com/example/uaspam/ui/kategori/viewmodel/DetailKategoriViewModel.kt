9c54f93f2a3941519b1a61238a68a32a89c9a4ac
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:18:48 +0700

    membuat data class detail ui state dan kategori ui event

--- app/src/main/java/com/example/uaspam/ui/kategori/viewmodel/DetailKategoriViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/kategori/viewmodel/DetailKategoriViewModel.kt
@@ -63,3 +63,30 @@ class DetailKategoriViewModel(
 }
 
 
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Kategori.toDetailUiEvent(): InsertUiEvent {
+    return InsertUiEvent(
+        idKategori = idKategori,
+        namaKategori = namaKategori,
+        deskripsiKategori = deskripsiKategori
+    )
+}
+
+data class KategoriUiEvent(
+    val idKategori : String = "",
+    val namaKategori : String = "",
+    val deskripsiKategori : String = ""
+
+)
