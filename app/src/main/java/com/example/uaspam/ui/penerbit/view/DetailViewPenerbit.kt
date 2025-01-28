c2c3f8e3b0fb758ddd81413a219d045d047bc03f
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:04:41 +0700

    menambahkan objek destinasi detail untuk route ke halaman detail penerbit

---
+++ app/src/main/java/com/example/uaspam/ui/penerbit/view/DetailViewPenerbit.kt
@@ -0,0 +1,53 @@
+package com.example.uaspam.ui.penerbit.view
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.model.Penerbit
+import com.example.uaspam.ui.PenyediaViewModel
+import com.example.uaspam.ui.navigation.CostumeTopAppBar
+import com.example.uaspam.ui.navigation.DestinasiNavigasi
+import com.example.uaspam.ui.penerbit.viewmodel.DetailPenerbitViewModel
+import com.example.uaspam.ui.penerbit.viewmodel.DetailUiState
+import com.example.uaspam.ui.penerbit.viewmodel.toPenerbit
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Penerbit"
+    const val IDPENERBIT = "idPenerbit"
+    val routeWithArgs = "$route/{$IDPENERBIT}"
+}
+
