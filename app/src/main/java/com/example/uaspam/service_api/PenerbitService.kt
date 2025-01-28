149ce876f8e0ba5a1ee3a8315688262b484dca3e
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 16:00:33 +0700

    memperbarui endpoint di tabel Penerbit Service

--- app/src/main/java/com/example/uaspam/service_api/PenerbitService.kt
+++ app/src/main/java/com/example/uaspam/service_api/PenerbitService.kt
@@ -8,26 +8,26 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface PenerbitService {
     @Headers(
-        "Accept : application/json",
-        "Content-Type : application/json",
+        "Accept: application/json",
+        "Content-Type: application/json",
     )
 
-    @POST("penerbit")
+    @POST("penerbit/store")
     suspend fun insertPenerbit(@Body penerbit: Penerbit)
 
     @GET("penerbit")
     suspend fun getAllPenerbit(): List<Penerbit>
 
-    @GET("penerbit/:id")
-    suspend fun getPenerbitbyId(@Query("id") id:String): Penerbit
+    @GET("penerbit/{id_penerbit}")
+    suspend fun getPenerbitbyId(@Path("id_penerbit") id:String): Penerbit
 
-    @PUT("penerbit/:id")
-    suspend fun updatePenerbit(@Query("id") id:String, @Body penerbit: Penerbit)
+    @PUT("penerbit/{id_penerbit}")
+    suspend fun updatePenerbit(@Path("id_penerbit") id:String, @Body penerbit: Penerbit)
 
-    @DELETE("penerbit/:id")
-    suspend fun deletePenerbit(@Query("id") id:String): Response<Void>
+    @DELETE("penerbit/{id_penerbit}")
+    suspend fun deletePenerbit(@Path("id_penerbit") id:String): Response<Void>
 }
