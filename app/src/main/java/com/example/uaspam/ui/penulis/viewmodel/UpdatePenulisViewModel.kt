81a282a803e1222120d2922f6ec95d7f4cf7a33c
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 19:42:59 +0700

    membuat class update , menambahkan State untuk menyimpan data input yang sedang diperbarui dan mengambil data dengan id

--- app/src/main/java/com/example/uaspam/ui/penulis/viewmodel/UpdatePenulisViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/penulis/viewmodel/UpdatePenulisViewModel.kt
@@ -10,6 +10,26 @@ import com.example.uaspam.repository.PenulisRepository
 import com.example.uaspam.ui.penulis.view.DestinasiUpdate
 import kotlinx.coroutines.launch
 
+class UpdatePenulisViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val penulisRepository: PenulisRepository)
+    : ViewModel() {
+
+        var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+                private val _idPenulis: String = checkNotNull(savedStateHandle[DestinasiUpdate.IDPENULIS])
+
+        init {
+            viewModelScope.launch {
+                try {
+                    updateUiState = penulisRepository.getPenulisbyId(_idPenulis)
+                        .toUiStatePenulis()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
 
         fun updateInsertPenulisState(insertUiEvent: InsertUiEvent) {
             updateUiState = updateUiState.copy(insertUiEvent = insertUiEvent)
