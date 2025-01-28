2f89db4a88bdc133f963d1c78ee7d99a649130f4
tiarahasnabila <tiarahasnabila@gmail.com>
2025-01-28 15:55:53 +0700

    memperbarui endpoint di tabel Penulis Service

--- app/src/main/java/com/example/uaspam/service_api/PenulisService.kt
+++ app/src/main/java/com/example/uaspam/service_api/PenulisService.kt
@@ -8,27 +8,28 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PenulisService {
     @Headers(
-        "Accept : application/json",
-        "Content-Type : application/json",
+        "Accept: application/json",
+        "Content-Type: application/json",
     )
 
-    @POST("penulis")
+    @POST("penulis/store")
     suspend fun insertPenulis(@Body penulis: Penulis)
 
     @GET("penulis")
     suspend fun getAllPenulis(): List<Penulis>
 
-    @GET("penulis/:id")
-    suspend fun getPenulisbyId(@Query("id") id:String): Penulis
+    @GET("penulis/{id_penulis}")
+    suspend fun getPenulisbyId(@Path("id_penulis") id:String): Penulis
 
-    @PUT("penulis/:id")
-    suspend fun updatePenulis(@Query("id") id:String, @Body penulis: Penulis)
+    @PUT("penulis/{id_penulis}")
+    suspend fun updatePenulis(@Path("id_penulis") id:String, @Body penulis: Penulis)
 
-    @DELETE("penulis/:id")
-    suspend fun deletePenulis(@Query("id") id:String): Response<Void>
+    @DELETE("penulis/{id_penulis}")
+    suspend fun deletePenulis(@Path("id_penulis") id:String): Response<Void>
 
 }
